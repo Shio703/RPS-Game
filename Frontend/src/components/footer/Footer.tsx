f5980a6da94b4cd75d4fff71bf959af5719e33b6
Shio703 <shiodjamurashvili26@gmail.com>
2025-10-22 21:54:39 +0400

    feat: Minimalistic footer added

--- Frontend/src/components/footer/Footer.tsx
+++ Frontend/src/components/footer/Footer.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Link from "next/link";
+import styles from "./footer.module.css";
+
+const year = new Date().getFullYear();
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
-  )
-}
+    <footer className={styles.footerCont}>
+      <p>
+        @{year}{" "}
+        <Link href="https://www.linkedin.com/in/shio-zhamurashvili">
+          Shio703
+        </Link>
+        , All Rights Reserved.
+      </p>
+      <Link href="https://github.com/Shio703/RPS-Game">Github Repository</Link>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
